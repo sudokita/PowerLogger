36bde75dff06f341112c84f64b85fa5c8b690ff6
Taylor Kisor-Smith <tkisorsm@uci.edu>
2013-10-29 17:55:33 -0700

    Initial commit of an empty Xcode workspace.

---
+++ PowerLogger/PowerLogger/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PowerLogger
+//
+//  Created by Taylor Kisor-Smith on 10/29/13.
+//  Copyright (c) 2013 Taylor Kisor-Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
